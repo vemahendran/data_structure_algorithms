6c4908d847ac99daf2b554a1e42f5d8c3a300ba7
Venkatesan Mahendran <venkatesan.mahendran@gmail.com>
2022-08-19 16:58:19 +0530

    Update island-count.js

--- javascript/graph/island-count.js
+++ javascript/graph/island-count.js
@@ -1,3 +1,10 @@
+/**
+ * Write a function, islandCount, that takes in a grid containing Ws and Ls.
+ * W represents water and L represents land. The function should return the
+ * number of islands on the grid. An island is a vertically or horizontally
+ * connected region of land.
+ */
+
 const islandCount = (grid) => {
     let count = 0;
     const visited = new Set();
