15d7cc741303a1df278d261e3529aa2d04ef1718
Venkatesan Mahendran <venkatesan.mahendran@gmail.com>
2022-08-30 19:19:30 +0530

    Update max-path-sum.js

--- javascript/binary-tree/max-path-sum.js
+++ javascript/binary-tree/max-path-sum.js
@@ -20,60 +20,87 @@ const maxPathSumR = (root) => {
 
 // Using iteration
 const maxPathSum = (root) => {
-    return 0;
+    if (root === null) return 0;
+
+    let max = -10000;
+    let stack = [root];
+
+    while (stack.length > 0) {
+        let current = stack.pop();
+        if (current.val > max) max = current.val;
+
+        if (current.left !== null) {
+            stack.push(current.left);
+            current.left.val += current.val;
+        }
+
+        if (current.right !== null) {
+            stack.push(current.right);
+            current.right.val += current.val;
+        }
+    }
+
+    return max;
+
 };
+    //        -1
+    //      /   \
+    //    -6    -5
+    //   /  \     \
+    // -3   0    -13
+    //     /       \
+    //    -1       -2
 
+// const test_00 = (() => {
+//     const a = new Node(3);
+//     const b = new Node(11);
+//     const c = new Node(4);
+//     const d = new Node(4);
+//     const e = new Node(-2);
+//     const f = new Node(1);
 
-const test_00 = (() => {
-    const a = new Node(3);
-    const b = new Node(11);
-    const c = new Node(4);
-    const d = new Node(4);
-    const e = new Node(-2);
-    const f = new Node(1);
+//     a.left = b;
+//     a.right = c;
+//     b.left = d;
+//     b.right = e;
+//     c.right = f;
 
-    a.left = b;
-    a.right = c;
-    b.left = d;
-    b.right = e;
-    c.right = f;
+//     //       3
+//     //    /    \
+//     //   11     4
+//     //  / \      \
+//     // 4   -2     1
 
-    //       3
-    //    /    \
-    //   11     4
-    //  / \      \
-    // 4   -2     1
+//     console.log(maxPathSum(a)); // -> 18
 
-    console.log(maxPathSum(a)); // -> 18
+// })();
 
-})();
+// const test_01 = (() => {
+//     const a = new Node(5);
+//     const b = new Node(11);
+//     const c = new Node(54);
+//     const d = new Node(20);
+//     const e = new Node(15);
+//     const f = new Node(1);
+//     const g = new Node(3);
 
-const test_01 = (() => {
-    const a = new Node(5);
-    const b = new Node(11);
-    const c = new Node(54);
-    const d = new Node(20);
-    const e = new Node(15);
-    const f = new Node(1);
-    const g = new Node(3);
+//     a.left = b;
+//     a.right = c;
+//     b.left = d;
+//     b.right = e;
+//     e.left = f;
+//     e.right = g;
 
-    a.left = b;
-    a.right = c;
-    b.left = d;
-    b.right = e;
-    e.left = f;
-    e.right = g;
+//     //        5
+//     //     /    \
+//     //    11    54
+//     //  /   \
+//     // 20   15
+//     //      / \
+//     //     1  3
 
-    //        5
-    //     /    \
-    //    11    54
-    //  /   \
-    // 20   15
-    //      / \
-    //     1  3
-
-    console.log(maxPathSum(a)); // -> 59
-})();
+//     console.log(maxPathSum(a)); // -> 59
+// })();
 
 const test_02 = (() => {
     const a = new Node(-1);
@@ -104,10 +131,10 @@ const test_02 = (() => {
     console.log(maxPathSum(a)); // -> -8
 })();
 
-const test_03 = (() => {
-    const a = new Node(42);
+// const test_03 = (() => {
+//     const a = new Node(42);
 
-    //        42
+//     //        42
 
-    console.log(maxPathSum(a)); // -> 42
-})();
+//     console.log(maxPathSum(a)); // -> 42
+// })();
